972b26e4fd2194cd0cd966aa86947e5ffa10635f
erics1996 <erics1996@yeah.net>
2020-11-16 22:32:55 +0800

    Modify the tab order of the control.

--- 2.QtDesigner/修改控件的Tab顺序/RunMainWinTabOrder.py
+++ 2.QtDesigner/修改控件的Tab顺序/RunMainWinTabOrder.py
@@ -3,11 +3,11 @@
 @author: qq3330447288
 @contact: erics1996@yeah.net
 @software: PyCharm
-@file: RunMainWinVHLayout.py.py
+@file: RunMainWinTabOrder.py
 @time: 2020/11/16 下午10:30
 """
 import sys
-import MainWinVHLayout
+import MainWinTabOrder
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 if __name__ == '__main__':
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     # 创建一个主窗口
     mainWindow = QMainWindow()
     # 创建Ui_MainWindow的实例
-    ui = MainWinVHLayout.Ui_MainWindow()
+    ui = MainWinTabOrder.Ui_MainWindow()
     # 调用setupUi在指定窗口(主窗口)中添加控件
     ui.setupUi(mainWindow)
     # 显示窗口
