e773c3e71ca042b5e73f669ed26525dccdf1a7e5
erics1996 <erics1996@yeah.net>
2020-11-17 16:24:46 +0800

    Set the icons for windows and applications.

--- 3.PyQt5核心API的使用/5.设置窗口和应用程序图标/WinAndApplicationIcon.py
+++ 3.PyQt5核心API的使用/5.设置窗口和应用程序图标/WinAndApplicationIcon.py
@@ -3,7 +3,7 @@
 @author: qq3330447288
 @contact: erics1996@yeah.net
 @software: PyCharm
-@file: WindowAndApplicationIcon.py
+@file: WinAndApplicationIcon.py.py
 @time: 2020/11/17 上午11:33
 """
 import sys
@@ -11,30 +11,27 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 from PyQt5.QtGui import QIcon
 
 
-class FirstMainWin(QMainWindow):
+class WinAndApplicationIcon(QMainWindow):
     def __init__(self):
-        """
-        初始化
-        :param parent:控件放到parent
-        """
-        super(FirstMainWin, self).__init__()
+        super(WinAndApplicationIcon, self).__init__()
+        self.init_ui()
+
+    def init_ui(self):
         # 设置主窗口的标题
-        self.setWindowTitle('第一个窗口应用')
-        # 设置窗口的尺寸
-        self.resize(400, 300)
-        # 获取当前的状态栏(默认是有状态栏的)
-        self.status = self.statusBar()
-        # 设置状态栏显示的消息(消息只存在5s)
-        self.status.showMessage('只显示5s的消息！', 5000)
+        self.setWindowTitle('设置窗口图标')
+        # 同时设置主窗口的尺寸和位置
+        self.setGeometry(400, 400, 300, 200)
+        # 设置窗口图标
+        self.setWindowIcon(QIcon('../images/Basilisk.ico'))
 
 
 if __name__ == '__main__':
     # 创建一个应用程序对象(传入参数)
     app = QApplication(sys.argv)
     # 设置应用程序的图标
-    app.setWindowIcon(QIcon('../images/Dragon.ico'))
+    app.setWindowIcon(QIcon('../images/Dragon.ico'))  # 可以用来设置主窗口的图标和应用程序的图标，但是如果主窗口已经设置了的图标，这里只能用于设置应用程序的图标
     # 创建窗口类的对象
-    main = FirstMainWin()
+    main = WinAndApplicationIcon()
     # 显示窗口
     main.show()
     # 调用exit()进入程序的主循环
